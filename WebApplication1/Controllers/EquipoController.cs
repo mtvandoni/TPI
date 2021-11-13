e3453128a6960282e3ccd9ab11665c0a6a9d7cf2
WALTER OSCAR GUGLIELMETTI <wguglielmetti@alumno.unlam.edu.ar>
2021-11-13 10:25:10 -0300

    Api Equipo

---
+++ WebApplication1/Controllers/EquipoController.cs
@@ -0,0 +1,111 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Authorization;
+using WebApplication1.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace WebApplication1.Controllers
+{
+    [Authorize]
+    [Route("api/[controller]")]
+    [ApiController]
+    public class EquipoController : ControllerBase
+    {
+        private readonly TPI_DBContext context;
+
+        public EquipoController(TPI_DBContext context)
+        {
+            this.context = context;
+
+        }
+
+        [AllowAnonymous]
+        // GET: api/<EquipoController>
+        [HttpGet]
+        public ActionResult<string> Get()
+        {
+            try {
+                return Ok(context.Equipos.ToList());
+            }
+            catch (Exception ex) {
+                return BadRequest(ex.Message);
+            }
+        }
+
+        // GET api/<EquipoController>/5
+        [HttpGet("{id}", Name = "getequipo")]
+        public ActionResult Get(int id)
+        {
+            try {
+                var cursada = context.Equipos.FirstOrDefault(p => p.IdEquipo == id);
+                return Ok(cursada);
+            }
+            catch (Exception ex) {
+                return BadRequest(ex.Message);
+            }
+        }
+
+        [HttpPost]
+        public ActionResult Post([FromBody] Equipo equipo)
+        {
+            try {
+                var equipoExist = context.Equipos.FirstOrDefault(p => p.IdEquipo == equipo.IdEquipo);
+                if (equipoExist != null) {
+                    return BadRequest("Equipo existente");
+                }
+                else {
+                    context.Equipos.Add(equipo);
+                    context.SaveChanges();
+                    return CreatedAtRoute("Getequipo", new { id = equipo.IdEquipo }, equipo);
+                }
+            }
+            catch (Exception ex) {
+                return BadRequest("Error al insertar el registro, validar los datos requeridos");
+            }
+        }
+
+        // PUT api/<EquipoController>/5
+        [HttpPut("{id}")]
+        public ActionResult Put(int id, [FromBody] Equipo equipo)
+        {
+            try {
+                if (equipo.IdEquipo == id) {
+                    context.Entry(equipo).State = EntityState.Modified;
+                    context.SaveChanges();
+                    return CreatedAtRoute("Getequipo", new { id = equipo.IdEquipo }, equipo);
+                }
+                else {
+                    return BadRequest();
+                }
+            }
+            catch (Exception ex) {
+                return BadRequest(ex.Message);
+            }
+        }
+
+        // DELETE api/<EquipoController>/5
+        [HttpDelete("{id}")]
+        public ActionResult Delete(int id)
+        {
+            try {
+                var equipo = context.Equipos.FirstOrDefault(p => p.IdEquipo == id);
+                if (equipo != null) {
+                    context.Equipos.Remove(equipo);
+                    context.SaveChanges();
+                    return Ok(id);
+                }
+                else {
+                    return BadRequest();
+                }
+            }
+            catch (Exception ex) {
+                return BadRequest(ex.Message);
+            }
+        }
+
+    }
+}
